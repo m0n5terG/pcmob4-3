20f2b3f26aca16fd72e5313d1a0f6214a49519ac
m0n5terG <mikotai@yahoo.com>
2021-05-22 14:26:33 +0800

    task5

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import ChatScreen from "./screens/chatScreen";
+import LoginScreen from "./screens/loginScreen";
+
+const Stack = createStackNavigator();
    
   export default function App() {
     return (
@@ -9,10 +13,10 @@ import { NavigationContainer } from "@react-navigation/native";
         <Stack.Navigator>
           <Stack.Screen
             name="Login"
-            component={loginScreen}
+            component={LoginScreen}
             options={{ headerShown: false }}
           />
-          <Stack.Screen
+          <Stack.Screen              
             name="Chat" 
             component={ChatScreen}
           />
@@ -20,7 +24,7 @@ import { NavigationContainer } from "@react-navigation/native";
       </NavigationContainer>
     );
    }
-   
+   /*
    const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -29,4 +33,4 @@ import { NavigationContainer } from "@react-navigation/native";
       justifyContent: "center",
     },
    });
-   
+   */
